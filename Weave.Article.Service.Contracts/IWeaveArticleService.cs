c5cec4d86d2ca5653f5b86ff78a6d660ce233c50
Arash <aemami99@gmail.com>
2013-05-26 16:15:51 -0700

    moved the Weave.Article service definition (contract) and DTOs to the portable folder

---
+++ Weave.Article.Service.Contracts/IWeaveArticleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Incoming = Weave.Article.Service.DTOs.ServerIncoming;
+using Outgoing = Weave.Article.Service.DTOs.ServerOutgoing;
+
+namespace Weave.Article.Service.Contracts
+{
+    public interface IWeaveArticleService
+    {
+        Task<bool> MarkRead(Guid userId, Incoming.SavedNewsItem newsItem);
+        Task RemoveRead(Guid userId, Guid newsItemId);
+        Task<List<Outgoing.SavedNewsItem>> GetRead(Guid userId, int take, int skip = 0);
+
+        Task<bool> AddFavorite(Guid userId, Incoming.SavedNewsItem newsItem);
+        Task RemoveFavorite(Guid userId, Guid newsItemId);
+        Task<List<Outgoing.SavedNewsItem>> GetFavorites(Guid userId, int take, int skip = 0);
+    }
+}
