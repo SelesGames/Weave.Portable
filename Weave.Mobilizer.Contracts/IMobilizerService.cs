6f3d47823e89ccfb97ed8d496e943080c479dc46
Arash <aemami99@gmail.com>
2013-05-31 01:34:01 -0700

    added Mobilizer service contracts and DTOs to the Portable folder

---
+++ Weave.Mobilizer.Contracts/IMobilizerService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Weave.Mobilizer.DTOs;
+
+namespace Weave.Mobilizer.Contracts
+{
+    public interface IMobilizerService
+    {
+        Task<ReadabilityResult> Get(string url);
+        Task Post(string url, ReadabilityResult article);
+    }
+}
