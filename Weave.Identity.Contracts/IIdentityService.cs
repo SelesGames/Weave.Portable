b1fa9212e786e8a9492a9d2b544607601dc31869
Arash <aemami99@gmail.com>
2013-06-21 01:54:50 -0700

    added Weave Identity portable classes and projects

---
+++ Weave.Identity.Contracts/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Weave.Identity.DTOs;
+
+namespace Weave.Identity.Contracts
+{
+    public interface IIdentityService
+    {
+        Task<IdentityInfo> GetUserFromFacebookToken(string facebookToken);
+        Task<IdentityInfo> GetUserFromTwitterToken(string twitterToken);
+        Task<IdentityInfo> GetUserFromMicrosoftToken(string microsoftToken);
+        Task<IdentityInfo> GetUserFromGoogleToken(string googleToken);
+        Task Add(IdentityInfo user);
+        Task Update(IdentityInfo user);
+    }
+}
