ed36cf9fae56875cd987d2956244744abb714d4c
Arash <aemami99@gmail.com>
2013-06-23 19:20:48 -0700

    added parameter to mobilizer get

--- Weave.Mobilizer.Contracts/IMobilizerService.cs
+++ Weave.Mobilizer.Contracts/IMobilizerService.cs
@@ -5,7 +5,7 @@ namespace Weave.Mobilizer.Contracts
 {
     public interface IMobilizerService
     {
-        Task<ReadabilityResult> Get(string url);
+        Task<ReadabilityResult> Get(string url, bool stripLeadImage);
         Task Post(string url, ReadabilityResult article);
     }
 }
