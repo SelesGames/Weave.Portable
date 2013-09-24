a0900a4bbd1c22f8e5a428f5f87d7dd423fdc573
Arash Emami <aemami99@gmail.com>
2013-09-23 22:48:05 -0700

    added IconUri property to Feed

--- Weave.User.Service.DTOs/ServerOutgoing/Feed.cs
+++ Weave.User.Service.DTOs/ServerOutgoing/Feed.cs
@@ -12,16 +12,18 @@ namespace Weave.User.Service.DTOs.ServerOutgoing
         [DataMember(Order= 1)]  public Guid Id { get; set; }
         [DataMember(Order= 2)]  public string Uri { get; set; }
         [DataMember(Order= 3)]  public string Name { get; set; }
-        [DataMember(Order= 4)]  public string Category { get; set; }
-        [DataMember(Order= 5)]  public ArticleViewingType ArticleViewingType { get; set; }
-        [DataMember(Order= 6)]  public int NewArticleCount { get; set; }
-        [DataMember(Order= 7)]  public int UnreadArticleCount { get; set; }
-        [DataMember(Order= 8)]  public int TotalArticleCount { get; set; }
-        [DataMember(Order= 9)]  public string TeaserImageUrl { get; set; }
+        [DataMember(Order= 4)]  public string IconUri { get; set; }
+
+        [DataMember(Order= 5)]  public string Category { get; set; }
+        [DataMember(Order= 6)]  public ArticleViewingType ArticleViewingType { get; set; }
+        [DataMember(Order= 7)]  public int NewArticleCount { get; set; }
+        [DataMember(Order= 8)]  public int UnreadArticleCount { get; set; }
+        [DataMember(Order= 9)]  public int TotalArticleCount { get; set; }
+        [DataMember(Order=10)]  public string TeaserImageUrl { get; set; }
 
         // delete for production
-        [DataMember(Order=10)]  public DateTime LastRefreshedOn { get; set; }
-        [DataMember(Order=11)]  public DateTime MostRecentEntrance { get; set; }
-        [DataMember(Order=12)]  public DateTime PreviousEntrance { get; set; }
+        [DataMember(Order=11)]  public DateTime LastRefreshedOn { get; set; }
+        [DataMember(Order=12)]  public DateTime MostRecentEntrance { get; set; }
+        [DataMember(Order=13)]  public DateTime PreviousEntrance { get; set; }
     }
 }
