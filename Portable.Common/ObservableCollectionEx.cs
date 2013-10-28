68989068c5890aca976e32ab7737727aa9512352
Arash Emami <aemami99@gmail.com>
2013-10-27 18:41:00 -0700

    added Portable.Common.csproj

---
+++ Portable.Common/ObservableCollectionEx.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Portable.Common.Collections
+{
+    public class ObservableCollectionEx<T> : ObservableCollection<T>
+    {
+        bool disablePropertyChanged = false;
+
+        public void AddRange(IEnumerable<T> range)
+        {
+            var items = range.ToList();
+
+            disablePropertyChanged = true;
+
+            foreach (var o in items)
+                this.Add(o);
+
+            disablePropertyChanged = false;
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items));
+        }
+
+        public void ClearAndAddRange(IEnumerable<T> range)
+        {
+            var newItems = range.ToList();
+            var oldItems = this.ToList();
+
+            disablePropertyChanged = true;
+
+            this.Clear();
+
+            foreach (var o in newItems)
+                this.Add(o);
+
+            disablePropertyChanged = false;
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItems, oldItems));
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (disablePropertyChanged)
+                return;
+
+            base.OnCollectionChanged(e);
+        }
+    }
+}
