662a97a0f950a8e23cd1dd924bc309afbf9c2fb6
Arash Emami <aemami99@gmail.com>
2013-11-11 18:49:10 -0800

    added DebugEx.cs

---
+++ Portable.Common/DebugEx.cs
@@ -0,0 +1,23 @@
+﻿
+public class DebugEx
+{
+    public static void WriteLine(string format, params object[] args)
+    {
+#if DEBUG
+        if (!System.Diagnostics.Debugger.IsAttached)
+            return;
+
+        string threadId = System.Environment.CurrentManagedThreadId.ToString();
+        string timestamp = string.Format("THREAD: {0}, AT: {1}", threadId, System.DateTime.Now.ToString("hh:mm:ss.fff tt"));
+        System.Diagnostics.Debug.WriteLine(timestamp + "\t" + format, args);
+#endif
+    }
+
+    public static void WriteLine(object o)
+    {
+#if DEBUG
+        if (o != null)
+            WriteLine(o.ToString());
+#endif
+    }
+}
