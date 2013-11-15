e405a8f187c9afa84fe9680500beb6a8c2368510
Arash Emami <aemami99@gmail.com>
2013-11-14 16:29:54 -0800

    cleaned up IIdentityService.cs, added a method for formatting Exceptions to multi-line strings

---
+++ Portable.Common/Extension Methods/ExceptionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace System
+{
+    public static class ExceptionExtensions
+    {
+        public static string FormatAsString(this Exception e)
+        {
+            var sb = new StringBuilder();
+            WriteExceptionToStringBuilder(e, sb);
+            return sb.ToString();
+        }
+
+        static void WriteExceptionToStringBuilder(Exception e, StringBuilder sb)
+        {
+            if (e == null)
+            {
+                sb.AppendLine("null");
+                return;
+            }
+
+            sb.AppendLine("Type:");
+            sb.AppendLine(e.GetType().FullName);
+            sb.AppendLine();
+
+            sb.AppendLine("Message:");
+            sb.AppendLine(e.Message);
+            sb.AppendLine();
+
+            sb.AppendLine("StackTrace:");
+            sb.AppendLine(e.StackTrace);
+            sb.AppendLine();
+
+            sb.AppendLine("Inner Exception:");
+            WriteExceptionToStringBuilder(e.InnerException, sb);
+        }
+    }
+}
--- Weave.Identity.Service.Contracts/IIdentityService.cs
+++ Weave.Identity.Service.Contracts/IIdentityService.cs
@@ -8,14 +8,7 @@ namespace Weave.Identity.Service.Contracts
     {
         Task<IdentityInfo> GetUserById(Guid userId);
 
-        //Task<IdentityInfo> GetUserFromFacebookToken(string facebookToken);
-        //Task<IdentityInfo> GetUserFromTwitterToken(string twitterToken);
-        //Task<IdentityInfo> GetUserFromMicrosoftToken(string microsoftToken);
-        //Task<IdentityInfo> GetUserFromGoogleToken(string googleToken);
-
         //Task<IdentityInfo> GetUserFromUserNameAndPassword(string username, string password);
-        //Task Add(IdentityInfo user);
-        //Task Update(IdentityInfo user);
 
         Task<IdentityInfo> SyncFacebook(Guid userId, string facebookToken);
         Task<IdentityInfo> SyncTwitter(Guid userId, string twitterToken);
