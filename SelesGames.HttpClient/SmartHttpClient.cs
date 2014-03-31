9e60938289c13535f2911268333d098f7ae4ec40
Arash Emami <aemami99@gmail.com>
2014-03-30 22:43:58 -0700

    very large refactor of SmartHttpClient.cs - basically added robust RetryPolicy support

--- SelesGames.HttpClient/SmartHttpClient.cs
+++ SelesGames.HttpClient/SmartHttpClient.cs
@@ -1,120 +1,13 @@
-﻿//using Common.Net.Http.Compression;
-//using System.Net.Http;
-//using System.Net.Http.Formatting;
-//using System.Net.Http.Headers;
-//using System.Threading;
-//using System.Threading.Tasks;
-
-//namespace SelesGames.HttpClient
-//{
-//    /// <summary>
-//    /// A version of HttpClient that reads the Content-Type header to deserialize a web request response into an object.  
-//    /// It also auto-decompresses responses.
-//    /// 
-//    /// By default, requests compressed Json objects
-//    /// </summary>
-//    public class SmartHttpClient : System.Net.Http.HttpClient
-//    {
-//        MediaTypeFormatterCollection formatters;
-//        ContentEncoderSettings encoderSettings;
-//        CompressionSettings compressionSettings;
-
-//        public MediaTypeFormatterCollection Formatters { get { return formatters; } }
-
-
-
-
-//        #region Constructors
-
-//        public SmartHttpClient(CompressionSettings compressionSettings = CompressionSettings.OnRequest | CompressionSettings.OnContent)
-//            : this(new StandardMediaTypeFormatters(), ContentEncoderSettings.Default, compressionSettings) { }
-
-//        public SmartHttpClient(ContentEncoderSettings encoderSettings, CompressionSettings compressionSettings = CompressionSettings.OnRequest | CompressionSettings.OnContent)
-//            : this(new StandardMediaTypeFormatters(), encoderSettings, compressionSettings) { }
-
-//        SmartHttpClient(
-//            MediaTypeFormatterCollection formatters,
-//            ContentEncoderSettings encoderSettings,
-//            CompressionSettings compressionSettings = CompressionSettings.OnRequest | CompressionSettings.OnContent)
-
-//            : base(new HttpClientCompressionHandler())
-//        {
-//            this.formatters = formatters;
-//            this.encoderSettings = encoderSettings;
-//            this.compressionSettings = compressionSettings;
-
-//            var accept = encoderSettings.Accept;
-//            if (!string.IsNullOrEmpty(accept))
-//                DefaultRequestHeaders.TryAddWithoutValidation("Accept", accept);
-
-//            if (compressionSettings.HasFlag(CompressionSettings.OnRequest))
-//                DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", new[] { "gzip", "deflate" });
-//        }
-
-//        #endregion
-
-
-
-
-//        #region Get
-
-//        public Task<T> GetAsync<T>(string url)
-//        {
-//            return GetAsync<T>(url, CancellationToken.None);
-//        }
-
-//        public async Task<T> GetAsync<T>(string url, CancellationToken cancellationToken)
-//        {
-//            var response = await GetAsync(url, cancellationToken).ConfigureAwait(false);
-//            response.EnsureSuccessStatusCode2();
-
-//            return await response.ReadResponseContentAsync<T>(Formatters).ConfigureAwait(false);
-//        }
-
-//        #endregion
-
-
-
-
-//        #region Post
-
-//        public Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj)
-//        {
-//            return PostAsync<TPost, TResult>(url, obj, CancellationToken.None);
-//        }
-
-//        public async Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj, CancellationToken cancelToken)
-//        {
-//            var response = await PostAsync(url, obj, cancelToken);
-//            response.EnsureSuccessStatusCode2();
-
-//            return await response.ReadResponseContentAsync<TResult>(Formatters).ConfigureAwait(false);
-//        }
-
-//        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj, CancellationToken cancelToken)
-//        {
-//            var mediaType = new MediaTypeHeaderValue(encoderSettings.ContentType);
-//            var formatter = Formatters.FindWriteFormatter<T>(mediaType);
-
-//            var content = new ObjectContent<T>(obj, formatter, mediaType);
-
-//            return base.PostAsync(url, content, cancelToken);
-//        }
-
-//        #endregion
-//    }
-//}
-
-
-using Common.Compression;
-using Common.Net.Http.Compression;
+﻿using Common.Compression;
+using SelesGames.HttpClient.RetryPolicies;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace SelesGames.HttpClient
 {
@@ -132,11 +25,19 @@ namespace SelesGames.HttpClient
         readonly IEnumerable<string> acceptEncodings;
 
         public MediaTypeFormatterCollection Formatters { get { return formatters; } }
+        public Func<System.Net.Http.HttpClient> HttpClientFactory { get; set; }
 
+        public static IRetryPolicy DefaultRetryPolicy { get; set; }
 
+    
 
 
         #region Constructors
+
+        static SmartHttpClient()
+        {
+            DefaultRetryPolicy = new LinearRetry(TimeSpan.FromSeconds(1), 3);
+        }
 
         public SmartHttpClient(CompressionSettings compressionSettings = CompressionSettings.OnRequest | CompressionSettings.OnContent)
             : this(new StandardMediaTypeFormatters(), ContentEncoderSettings.Default, compressionSettings) { }
@@ -153,6 +54,8 @@ namespace SelesGames.HttpClient
             this.encoderSettings = encoderSettings;
             this.compressionSettings = compressionSettings;
             this.acceptEncodings = Settings.CompressionHandlers.GetSupportedEncodings().ToArray();
+
+            HttpClientFactory = () => new AutoCompressionClient();
         }
 
         #endregion
@@ -160,54 +63,26 @@ namespace SelesGames.HttpClient
 
 
 
-        #region Get
+        #region Create the HttpRequestMessage based on settings
 
-        public Task GetAsync(string url)
+        public HttpRequestMessage CreateRequest(HttpMethod method, string url)
         {
-            return CreateClient().GetAsync(url, CancellationToken.None);
+            var request = new HttpRequestMessage(method, url);
+
+            var accept = encoderSettings.Accept;
+            if (!string.IsNullOrEmpty(accept))
+                request.Headers.TryAddWithoutValidation("Accept", accept);
+
+            if (compressionSettings.HasFlag(CompressionSettings.OnRequest))
+                request.Headers.TryAddWithoutValidation("Accept-Encoding", acceptEncodings);
+
+            return request;
         }
 
-        public Task GetAsync(string url, CancellationToken cancelToken)
+        public HttpRequestMessage CreateRequest<T>(HttpMethod method, string url, T obj)
         {
-            return CreateClient().GetAsync(url, cancelToken);           
-        }
+            var request = CreateRequest(method, url);
 
-        public Task<T> GetAsync<T>(string url)
-        {
-            return GetAsync<T>(url, CancellationToken.None);
-        }
-
-        public async Task<T> GetAsync<T>(string url, CancellationToken cancellationToken)
-        {
-            var client = CreateClient();
-            var response = await client.GetAsync(url, cancellationToken).ConfigureAwait(false);
-            response.EnsureSuccessStatusCode2();
-
-            return await response.ReadResponseContentAsync<T>(Formatters).ConfigureAwait(false);
-        }
-
-        #endregion
-
-
-
-
-        #region Post
-
-        public Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj)
-        {
-            return PostAsync<TPost, TResult>(url, obj, CancellationToken.None);
-        }
-
-        public async Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj, CancellationToken cancelToken)
-        {
-            var response = await PostAsync(url, obj, cancelToken).ConfigureAwait(false);
-            response.EnsureSuccessStatusCode2();
-
-            return await response.ReadResponseContentAsync<TResult>(Formatters).ConfigureAwait(false);
-        }
-
-        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj, CancellationToken cancelToken)
-        {
             var mediaType = new MediaTypeHeaderValue(encoderSettings.ContentType);
             var formatter = Formatters.FindWriteFormatter<T>(mediaType);
 
@@ -216,26 +91,186 @@ namespace SelesGames.HttpClient
             //if (compressionSettings.HasFlag(CompressionSettings.OnContent))
             //    content.Headers.TryAddWithoutValidation("Content-Encoding", "gzip");
 
-            var client = CreateClient();
-            return client.PostAsync(url, content, cancelToken);
+            request.Content = content;
+            return request;
         }
 
         #endregion
 
 
 
-        System.Net.Http.HttpClient CreateClient()
+
+        #region Send the Request
+
+        internal async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, RetryContext retryContext, IRetryPolicy retryPolicy, CancellationToken cancellationToken)
         {
-            var client = new AutoCompressionClient();
+            if (retryPolicy == null)
+                throw new ArgumentNullException("retryPolicy in SmartHttpClient.SendAsync");
 
-            var accept = encoderSettings.Accept;
-            if (!string.IsNullOrEmpty(accept))
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", accept);
+            if (retryContext != null)
+            {
+                var retryInfo = retryPolicy.Evaluate(retryContext);
+                if (!retryInfo.ShouldRetry)
+                    throw new ErrorResponseException(retryContext.LastRequestResult);
 
-            if (compressionSettings.HasFlag(CompressionSettings.OnRequest))
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", acceptEncodings);
+                await Task.Delay(retryInfo.RetryInterval).ConfigureAwait(false);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
 
-            return client;
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine(
+                "Sending request... (attempt {2})\r\n{0}\r\n{1}\r\n\r\n",
+                request.ToString(),
+                request.Content,
+                retryContext == null ? 0 : retryContext.CurrentRetryCount);
+#endif
+
+            var client = HttpClientFactory();
+            var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            else
+            {
+                retryContext = retryContext ?? new RetryContext();
+
+                retryContext.CurrentRetryCount++;
+                retryContext.LastRequestResult = response;
+
+                request = Copy(request);
+                return await SendAsync(request, retryContext, retryPolicy, cancellationToken);
+            }
         }
+
+        HttpRequestMessage Copy(HttpRequestMessage original)
+        {
+            var request = new HttpRequestMessage(original.Method, original.RequestUri)
+            {
+                Content = original.Content,
+                Version = original.Version,
+            };
+
+            foreach (var header in original.Headers)
+            {
+                request.Headers.TryAddWithoutValidation(header.Key, header.Value);
+            }
+
+            foreach (var property in original.Properties)
+            {
+                request.Properties.Add(property.Key, property.Value);
+            }
+
+            return request;
+        }
+
+        #endregion
+
+
+
+
+        #region GET
+
+        public Task<HttpResponseMessage> GetAsync(string url, IRetryPolicy retryPolicy, CancellationToken cancellationToken)
+        {
+            return SendAsync(CreateRequest(HttpMethod.Get, url), null, retryPolicy, cancellationToken);
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string url, IRetryPolicy retryPolicy)
+        {
+            return GetAsync(url, retryPolicy, CancellationToken.None);
+        }
+        
+        public Task<HttpResponseMessage> GetAsync(string url, CancellationToken cancellationToken)
+        {
+            return GetAsync(url, DefaultRetryPolicy, cancellationToken);
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return GetAsync(url, DefaultRetryPolicy, CancellationToken.None);
+        }
+
+        public async Task<T> GetAsync<T>(string url, IRetryPolicy retryPolicy, CancellationToken cancellationToken)
+        {
+            var response = await GetAsync(url, retryPolicy, cancellationToken).ConfigureAwait(false);
+
+            cancellationToken.ThrowIfCancellationRequested();
+            response.EnsureSuccessStatusCode2();
+
+            return await response.ReadResponseContentAsync<T>(Formatters).ConfigureAwait(false);
+        }
+
+        public Task<T> GetAsync<T>(string url, IRetryPolicy retryPolicy)
+        {
+            return GetAsync<T>(url, retryPolicy, CancellationToken.None);
+        }
+
+        public Task<T> GetAsync<T>(string url, CancellationToken cancellationToken)
+        {
+            return GetAsync<T>(url, DefaultRetryPolicy, cancellationToken);
+        }
+
+        public Task<T> GetAsync<T>(string url)
+        {
+            return GetAsync<T>(url, DefaultRetryPolicy, CancellationToken.None);
+        }
+
+        #endregion
+
+
+
+
+        #region POST
+
+        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj, IRetryPolicy retryPolicy, CancellationToken cancellationToken)
+        {
+            return SendAsync(CreateRequest(HttpMethod.Post, url, obj), null, retryPolicy, cancellationToken);
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj, CancellationToken cancellationToken)
+        {
+            return PostAsync(url, obj, DefaultRetryPolicy, cancellationToken);
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj, IRetryPolicy retryPolicy)
+        {
+            return PostAsync(url, obj, retryPolicy, CancellationToken.None);
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string url, T obj)
+        {
+            return PostAsync(url, obj, DefaultRetryPolicy, CancellationToken.None);
+        }
+
+        public async Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj, IRetryPolicy retryPolicy, CancellationToken cancellationToken)
+        {
+            var response = await PostAsync(url, obj, retryPolicy, cancellationToken).ConfigureAwait(false);
+
+            cancellationToken.ThrowIfCancellationRequested();
+            response.EnsureSuccessStatusCode2();
+
+            return await response.ReadResponseContentAsync<TResult>(Formatters).ConfigureAwait(false);
+        }
+
+        public Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj, CancellationToken cancellationToken)
+        {
+            return PostAsync<TPost, TResult>(url, obj, DefaultRetryPolicy, cancellationToken);
+        }
+
+        public Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj, IRetryPolicy retryPolicy)
+        {
+            return PostAsync<TPost, TResult>(url, obj, retryPolicy, CancellationToken.None);
+        }
+
+        public Task<TResult> PostAsync<TPost, TResult>(string url, TPost obj)
+        {
+            return PostAsync<TPost, TResult>(url, obj, DefaultRetryPolicy, CancellationToken.None);
+        }
+
+        #endregion
     }
 }
