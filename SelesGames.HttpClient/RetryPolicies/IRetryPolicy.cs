2384542d2d556345ecc934f3a175f708bcb02ebc
Arash Emami <aemami99@gmail.com>
2014-03-30 22:42:08 -0700

    minor cleanup

--- SelesGames.HttpClient/RetryPolicies/IRetryPolicy.cs
+++ SelesGames.HttpClient/RetryPolicies/IRetryPolicy.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SelesGames.HttpClient.RetryPolicies
 {
     public interface IRetryPolicy
