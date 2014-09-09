3ec4a4ba2897f3d7ad2f032fddb617f56c42b95d
Arash Emami <aemami99@gmail.com>
2014-09-09 16:55:30 -0700

    changed the Json formatters handling of null values as well as datetime timezones

--- SelesGames.HttpClient/StandardMediaTypeFormatters.cs
+++ SelesGames.HttpClient/StandardMediaTypeFormatters.cs
@@ -1,9 +1,18 @@
 ﻿using System.Net.Http.Formatting;
+using System.Linq;
 
 namespace SelesGames.HttpClient
 {
     public class StandardMediaTypeFormatters : MediaTypeFormatterCollection
     {
-        public StandardMediaTypeFormatters() { }
+        public StandardMediaTypeFormatters()
+        {
+            if (!this.Any())
+                return;
+
+            var jsonFormatter = (JsonMediaTypeFormatter)this[0];
+            jsonFormatter.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+        }
     }
 }
